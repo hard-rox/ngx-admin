0a26f5d31888b76489740284a92fb95a23934e77
Rasedur Rahman Roxy <roxy.xprez@gmail.com>
2020-04-28 22:51:53 +0600

    Fixed perpush linting.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,17 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule, NbCardModule, NbLayoutModule, NbUserModule, NbActionsModule, NbSearchModule, NbSidebarModule, NbContextMenuModule, NbButtonModule, NbSelectModule, NbIconModule } from '@nebular/theme';
+import {
+  NbMenuModule,
+  NbCardModule,
+  NbLayoutModule,
+  NbUserModule,
+  NbActionsModule,
+  NbSearchModule,
+  NbSidebarModule,
+  NbContextMenuModule,
+  NbButtonModule,
+  NbSelectModule,
+  NbIconModule
+} from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
@@ -7,26 +19,7 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SmartTableComponent } from './smart-table/smart-table.component';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
-import { NbSecurityModule } from '@nebular/security';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { HeaderComponent, FooterComponent, SearchInputComponent } from '../components';
-
-const NB_MODULES = [
-  NbLayoutModule,
-  NbMenuModule,
-  NbUserModule,
-  NbActionsModule,
-  NbSearchModule,
-  NbSidebarModule,
-  NbContextMenuModule,
-  NbSecurityModule,
-  NbButtonModule,
-  NbSelectModule,
-  NbIconModule,
-  NbEvaIconsModule,
-  NbMenuModule,
-  NbCardModule,
-];
 
 const COMPONENTS = [
   HeaderComponent,
